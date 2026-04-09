ec39701d86ef9e792b7e7d5aeb85924e2ec8b7e3
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-04-09 20:36:02 +0200

    Update Avatar.jsx

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -3,9 +3,7 @@ import { avatarColor } from '../hooks/useChat'
 export default function Avatar({ name = '', src, size = 48, style = {} }) {
   const initials = name.split(' ').map(w => w[0]).slice(0, 2).join('').toUpperCase() || '?'
   const bg = avatarColor(name)
-
   if (src) return <img className="av-img" src={src} alt={name} style={{ width: size, height: size, ...style }} />
-
   return (
     <div className="av-fb" style={{ width: size, height: size, background: bg, fontSize: size * .36, ...style }}>
       {initials}
